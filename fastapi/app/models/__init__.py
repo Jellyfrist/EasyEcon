3f142906b04edbe970d5a8e921ad050c2767eeff
Jellyfrist <Natthanicha.forzen@gmail.com>
2026-02-26 17:49:49 +0700

    refactor(models): export new models in __init__

---
+++ fastapi/app/models/__init__.py
@@ -0,0 +1,11 @@
+from .user import User
+from .social_auth import SocialAuth
+
+from .course import Course
+from .module import Module
+from .flashcard import FlashcardSet, Flashcard, FlashcardProgress
+from .best_attempt import BestAttempt
+from .learning_page import LearningPage
+from .exam_template import ExamTemplate
+from .exam_session import ExamSession
+from .exam_attempt import ExamAttempt
