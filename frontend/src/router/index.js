5d6c2204a23524fcde0f3eadd5969c4709601138
Jellyfrist <Natthanicha.forzen@gmail.com>
2026-03-10 09:06:36 +0700

    fix(router): change file to correct name

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -80,7 +80,7 @@ const routes = [
   {
     path: '/admin',
     name: 'Admin',
-    component: () => import('@/views/Admin.vue'),
+    component: () => import('@/views/AdminPage.vue'),
     meta: {
       showNavbar: true,
       showFooter: true,
